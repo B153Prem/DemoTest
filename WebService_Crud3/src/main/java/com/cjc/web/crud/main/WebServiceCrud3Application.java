27b8d56ef0b4d9e2ed5d08d7a057aaee08ffd6df
Prem <sirsikarp@gmail.com>
2023-08-07 14:31:37 +0530

    modified by prem

--- WebService_Crud3/src/main/java/com/cjc/web/crud/main/WebServiceCrud3Application.java
+++ WebService_Crud3/src/main/java/com/cjc/web/crud/main/WebServiceCrud3Application.java
@@ -11,6 +11,7 @@ public class WebServiceCrud3Application {
 
 	public static void main(String[] args) {
 		System.out.println("This is Web Service Crud 3");
+		System.out.println("This is web modification to pranali");
 		SpringApplication.run(WebServiceCrud3Application.class, args);
 	}
 
